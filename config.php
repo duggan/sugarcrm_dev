53fa34f2f74269d2ab3ab504f103c0be7a10dc62
Ross Duggan <ross@orchestra-node.office.sf.engineyard.com>
2011-11-09 06:44:41 -0800

    config

---
+++ config.php
@@ -0,0 +1,303 @@
+<?php
+// created: 2011-11-11 15:41:07
+$sugar_config = array (
+  'admin_access_control' => false,
+  'admin_export_only' => false,
+  'cache_dir' => 'cache/',
+  'calculate_response_time' => true,
+  'chartEngine' => 'Jit',
+  'common_ml_dir' => '',
+  'create_default_user' => false,
+  'currency' => '',
+  'dashlet_display_row_options' => 
+  array (
+    0 => '1',
+    1 => '3',
+    2 => '5',
+    3 => '10',
+  ),
+  'date_formats' => 
+  array (
+    'Y-m-d' => '2010-12-23',
+    'm-d-Y' => '12-23-2010',
+    'd-m-Y' => '23-12-2010',
+    'Y/m/d' => '2010/12/23',
+    'm/d/Y' => '12/23/2010',
+    'd/m/Y' => '23/12/2010',
+    'Y.m.d' => '2010.12.23',
+    'd.m.Y' => '23.12.2010',
+    'm.d.Y' => '12.23.2010',
+  ),
+  'datef' => 'm/d/Y',
+  'dbconfig' => 
+  array (
+    'db_host_name' => '10.194.111.8',
+    'db_user_name' => 'user_8b471185',
+    'db_password' => 'eivLh$7;P5dV8(',
+    'db_name' => 'db_8b471185',
+    'db_type' => 'mysql',
+  ),
+  'dbconfigoption' => 
+  array (
+    'persistent' => true,
+    'autofree' => false,
+    'debug' => 0,
+    'seqname_format' => '%s_seq',
+    'portability' => 0,
+    'ssl' => false,
+  ),
+  'default_action' => 'index',
+  'default_charset' => 'UTF-8',
+  'default_currencies' => 
+  array (
+    'AUD' => 
+    array (
+      'name' => 'Australian Dollars',
+      'iso4217' => 'AUD',
+      'symbol' => '$',
+    ),
+    'BRL' => 
+    array (
+      'name' => 'Brazilian Reais',
+      'iso4217' => 'BRL',
+      'symbol' => 'R$',
+    ),
+    'GBP' => 
+    array (
+      'name' => 'British Pounds',
+      'iso4217' => 'GBP',
+      'symbol' => '£',
+    ),
+    'CAD' => 
+    array (
+      'name' => 'Canadian Dollars',
+      'iso4217' => 'CAD',
+      'symbol' => '$',
+    ),
+    'CNY' => 
+    array (
+      'name' => 'Chinese Yuan',
+      'iso4217' => 'CNY',
+      'symbol' => '￥',
+    ),
+    'EUR' => 
+    array (
+      'name' => 'Euro',
+      'iso4217' => 'EUR',
+      'symbol' => '€',
+    ),
+    'HKD' => 
+    array (
+      'name' => 'Hong Kong Dollars',
+      'iso4217' => 'HKD',
+      'symbol' => '$',
+    ),
+    'INR' => 
+    array (
+      'name' => 'Indian Rupees',
+      'iso4217' => 'INR',
+      'symbol' => '₨',
+    ),
+    'KRW' => 
+    array (
+      'name' => 'Korean Won',
+      'iso4217' => 'KRW',
+      'symbol' => '₩',
+    ),
+    'YEN' => 
+    array (
+      'name' => 'Japanese Yen',
+      'iso4217' => 'JPY',
+      'symbol' => '¥',
+    ),
+    'MXM' => 
+    array (
+      'name' => 'Mexican Pesos',
+      'iso4217' => 'MXM',
+      'symbol' => '$',
+    ),
+    'SGD' => 
+    array (
+      'name' => 'Singaporean Dollars',
+      'iso4217' => 'SGD',
+      'symbol' => '$',
+    ),
+    'CHF' => 
+    array (
+      'name' => 'Swiss Franc',
+      'iso4217' => 'CHF',
+      'symbol' => 'SFr.',
+    ),
+    'THB' => 
+    array (
+      'name' => 'Thai Baht',
+      'iso4217' => 'THB',
+      'symbol' => '฿',
+    ),
+    'USD' => 
+    array (
+      'name' => 'US Dollars',
+      'iso4217' => 'USD',
+      'symbol' => '$',
+    ),
+  ),
+  'default_currency_iso4217' => 'USD',
+  'default_currency_name' => 'US Dollars',
+  'default_currency_significant_digits' => 2,
+  'default_currency_symbol' => '$',
+  'default_date_format' => 'm/d/Y',
+  'default_decimal_seperator' => '.',
+  'default_email_charset' => 'UTF-8',
+  'default_email_client' => 'sugar',
+  'default_email_editor' => 'html',
+  'default_export_charset' => 'UTF-8',
+  'default_language' => 'en_us',
+  'default_locale_name_format' => 's f l',
+  'default_max_tabs' => '7',
+  'default_module' => 'Home',
+  'default_navigation_paradigm' => 'gm',
+  'default_number_grouping_seperator' => ',',
+  'default_password' => '',
+  'default_permissions' => 
+  array (
+    'dir_mode' => 1528,
+    'file_mode' => 432,
+    'user' => '',
+    'group' => '',
+  ),
+  'default_subpanel_links' => false,
+  'default_subpanel_tabs' => true,
+  'default_swap_last_viewed' => false,
+  'default_swap_shortcuts' => false,
+  'default_theme' => 'Sugar5',
+  'default_time_format' => 'h:ia',
+  'default_user_is_admin' => false,
+  'default_user_name' => '',
+  'demoData' => 'yes',
+  'disable_export' => false,
+  'disable_persistent_connections' => 'false',
+  'display_email_template_variable_chooser' => false,
+  'display_inbound_email_buttons' => false,
+  'dump_slow_queries' => false,
+  'email_default_client' => 'sugar',
+  'email_default_delete_attachments' => true,
+  'email_default_editor' => 'html',
+  'export_delimiter' => ',',
+  'history_max_viewed' => 50,
+  'host_name' => 'dummy-default',
+  'import_dir' => 'cache/import/',
+  'import_max_execution_time' => 3600,
+  'import_max_records_per_file' => 100,
+  'import_max_records_total_limit' => '',
+  'installer_locked' => true,
+  'js_custom_version' => '',
+  'js_lang_version' => 1,
+  'languages' => 
+  array (
+    'en_us' => 'English (US)',
+  ),
+  'large_scale_test' => false,
+  'lead_conv_activity_opt' => 'donothing',
+  'list_max_entries_per_page' => 20,
+  'list_max_entries_per_subpanel' => 10,
+  'lock_default_user_name' => false,
+  'lock_homepage' => false,
+  'lock_subpanels' => false,
+  'log_dir' => '.',
+  'log_file' => 'sugarcrm.log',
+  'log_memory_usage' => false,
+  'logger' => 
+  array (
+    'level' => 'fatal',
+    'file' => 
+    array (
+      'ext' => '.log',
+      'name' => 'sugarcrm',
+      'dateFormat' => '%c',
+      'maxSize' => '10MB',
+      'maxLogs' => 10,
+      'suffix' => '%m_%Y',
+    ),
+  ),
+  'max_dashlets_homepage' => '15',
+  'passwordsetting' => 
+  array (
+    'SystemGeneratedPasswordON' => '',
+    'generatepasswordtmpl' => '',
+    'lostpasswordtmpl' => '',
+    'forgotpasswordON' => false,
+    'linkexpiration' => '1',
+    'linkexpirationtime' => '30',
+    'linkexpirationtype' => '1',
+    'systexpiration' => '0',
+    'systexpirationtime' => '',
+    'systexpirationtype' => '0',
+    'systexpirationlogin' => '',
+  ),
+  'portal_view' => 'single_user',
+  'require_accounts' => true,
+  'resource_management' => 
+  array (
+    'special_query_limit' => 50000,
+    'special_query_modules' => 
+    array (
+      0 => 'Reports',
+      1 => 'Export',
+      2 => 'Import',
+      3 => 'Administration',
+      4 => 'Sync',
+    ),
+    'default_limit' => 1000,
+  ),
+  'rss_cache_time' => '10800',
+  'save_query' => 'all',
+  'session_dir' => '',
+  'showDetailData' => true,
+  'showThemePicker' => true,
+  'site_url' => 'http://dummy-default',
+  'slow_query_time_msec' => '100',
+  'sugar_version' => '6.3.0',
+  'sugarbeet' => true,
+  'time_formats' => 
+  array (
+    'H:i' => '23:00',
+    'h:ia' => '11:00pm',
+    'h:iA' => '11:00PM',
+    'h:i a' => '11:00 pm',
+    'h:i A' => '11:00 PM',
+    'H.i' => '23.00',
+    'h.ia' => '11.00pm',
+    'h.iA' => '11.00PM',
+    'h.i a' => '11.00 pm',
+    'h.i A' => '11.00 PM',
+  ),
+  'timef' => 'H:i',
+  'tmp_dir' => 'cache/xml/',
+  'tracker_max_display_length' => 15,
+  'translation_string_prefix' => false,
+  'unique_key' => '85e8db125dc420e56f172fb5987b5990',
+  'upload_badext' => 
+  array (
+    0 => 'php',
+    1 => 'php3',
+    2 => 'php4',
+    3 => 'php5',
+    4 => 'pl',
+    5 => 'cgi',
+    6 => 'py',
+    7 => 'asp',
+    8 => 'cfm',
+    9 => 'js',
+    10 => 'vbs',
+    11 => 'html',
+    12 => 'htm',
+  ),
+  'upload_dir' => 'cache/upload/',
+  'upload_maxsize' => 3000000,
+  'use_common_ml_dir' => false,
+  'use_real_names' => true,
+  'vcal_time' => '2',
+  'verify_client_ip' => true,
+);
+?>
+
