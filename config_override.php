aa665da0546a0135273aaa52dbb0aa016be84b72
Ross Duggan <ross@orchestra-node.office.sf.engineyard.com>
2011-11-11 08:08:36 -0800

    comment

--- config_override.php
+++ config_override.php
@@ -1,4 +1,4 @@
 <?php
-
+// A comment
 $sugar_config['http_referer']['list'][] = 'ec2-107-22-111-216.compute-1.amazonaws.com';
 $sugar_config['http_referer']['actions'] =array( 'index', 'ListView', 'DetailView', 'EditView', 'oauth', 'Authenticate', 'Login', 'SupportPortal', 'SetTimezone' );
