023753335db1ec694289029141f89cad41d4279c
Ross Duggan <ross@orchestra-node.office.sf.engineyard.com>
2011-11-11 08:05:34 -0800

    new host

--- config_override.php
+++ config_override.php
@@ -1,4 +1,4 @@
 <?php
 
-$sugar_config['http_referer']['list'][] = 'ec2-107-22-22-55.compute-1.amazonaws.com';
+$sugar_config['http_referer']['list'][] = 'ec2-107-22-111-216.compute-1.amazonaws.com';
 $sugar_config['http_referer']['actions'] =array( 'index', 'ListView', 'DetailView', 'EditView', 'oauth', 'Authenticate', 'Login', 'SupportPortal', 'SetTimezone' );
